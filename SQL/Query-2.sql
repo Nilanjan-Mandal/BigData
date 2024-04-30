1cc04a6a9a43a54f7b076acae65e5ab7066af0c8
Nilanjan Mandal <nmandalstu16@hotmail.com>
2024-05-01 00:58:21 +0530

    SQL folder is been deleted

--- SQL/Query-2.sql
+++
@@ -1,5 +0,0 @@
-2. List down all Olympics games held so far. (Data issue at 1956-"Summer"-"Stockholm")
-
-    select distinct oh.year,oh.season,oh.city
-    from olympics_history oh
-    order by year;
