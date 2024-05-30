99347c7ac5ee5a84d4be30059e376dbedebc9f8e
Nilanjan Mandal <nmandalstu16@hotmail.com>
2024-05-31 00:15:07 +0530

    Add

--- Spark/Programs/src/main/scala/com/example/Word_Count.scala
+++ Spark/Programs/src/main/scala/com/example/Word_Count.scala
@@ -15,10 +15,14 @@ object Word_Count extends App {
   val sc = spark.sparkContext
 
   val rdd = sc.textFile(path = "/Users/nilanjan/Documents/IT/Bigdata/Spark/folder/readfile.txt")
-  rdd.foreach(println)
+    //rdd.foreach(println)
+  val rdd1 = rdd.flatMap(x => x.split(" "))  // It will split the data based on the space
+  val rdd2 = rdd1.map(x => (x,1))
+  val rdd3 = rdd2.reduceByKey((x,y) => x+y)
+  rdd3.foreach(println)
 
 }
 
 
 // 1. We need to use sparkcontext to work on rdd.
-// 2. Type foreach(println) manually. 
+// 2. Type foreach(println) manually.
